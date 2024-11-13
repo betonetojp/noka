89e8a536236eae069c819d599ea71623564a2547
betoneto <109812892+betonetojp@users.noreply.github.com>
2024-11-14 03:57:05 +0900

    デフォルトリレーリストに新しいリレーを追加
    `LoadRelays`メソッド内で、デフォルトのリレーリストに新しいリレーが追加されました。
    - 有効なリレーとして `wss://relay-jp.nostr.wirednet.jp/` が追加されました。
    - 無効なリレーとして `wss://relay.nostr.band/` が追加されました。

--- noka/Tools.cs
+++ noka/Tools.cs
@@ -175,8 +175,10 @@ namespace noka
             List<Relay> defaultRelays = [
                 new Relay { Enabled = true, Url = "wss://yabu.me/" },
                 new Relay { Enabled = true, Url = "wss://r.kojira.io/" },
+                new Relay { Enabled = true, Url = "wss://relay-jp.nostr.wirednet.jp/" },
                 new Relay { Enabled = false, Url = "wss://nos.lol/" },
                 new Relay { Enabled = false, Url = "wss://relay.damus.io/" },
+                new Relay { Enabled = false, Url = "wss://relay.nostr.band/" },
                 ];
 
             // relays.jsonを読み込み
